ff842e9b967a45c50a995a4f3ba79c682eac97c7
Lena Lewkowicz <s30126@pjwstk.edu.pl>
2025-03-26 22:29:13 +0100

    finished code :D

--- cwiczenia3/src/Main.java
+++ cwiczenia3/src/Main.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        /* Student student1 = new Student("Jan", "Kowalski", "s12345", "jan@mail.com");
-        StudentGroup studentGroup1 = new StudentGroup();
+        Student student1 = new Student("Jan", "Kowalski", "s12345", "jan@mail.com",
+                "Ulica 123", new ArrayList<>());
+
+        student1.addGrade(5.0);
+        student1.addGrade(4.5);
+        student1.addGrade(4.0);
+        student1.addGrade(3.0);
 
         student1.getFinalGrade();
-        studentGroup1.addStudent(student1); */
+
+        StudentGroup group1 = new StudentGroup("14c", new ArrayList<>());
+        group1.addStudent(student1);
 
     }
 }
--- cwiczenia3/src/StudentGroup.java
+++ cwiczenia3/src/StudentGroup.java
@@ -5,14 +5,19 @@ public class StudentGroup {
     String nazwa;
     ArrayList<Student> students;
 
+    public StudentGroup(String nazwa, ArrayList<Student> students) {
+        this.nazwa = nazwa;
+        this.students = students;
+    }
+
     public void addStudent(Student student) {
-         students.add(student);
          if (students.size() == 15) {
              throw new IllegalStateException("The maximum number of students in one group is 15.");
          }
          if (students.contains(student)) {
              throw new IllegalStateException("This student is already in this group.");
          }
+        students.add(student);
     }
 
 }
